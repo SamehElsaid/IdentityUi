a87f3d9b17d42ab175ebd2931dd9ac9520ba514a
Sameh Elmeligy <109442419+SamehElsaid@users.noreply.github.com>
2026-09-17 23:04:04 +0300

    Update environment variables and localization settings

--- next.config.js
+++ next.config.js
@@ -2,30 +2,34 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 
-// Remove this if you're not using Fullcalendar features
-
 module.exports = {
   reactStrictMode: false,
+
   i18n: {
     locales: ['en', 'ar'],
     defaultLocale: 'en',
     localeDetection: true
   },
-    env: {
-    API_URL: 'https://cortextest.singleclic.com/IdentityServer/api',
-    IDENTITY_URL: 'https://cortextest.singleclic.com/IdentityServer/',
-    API_BASE_URL: 'https://cortextest.singleclic.com/LowCode/api',
+
+  env: {
+    API_URL: 'https://cortextest.singleclic.com/identityServerFRA/api',
+    IDENTITY_URL: 'https://cortextest.singleclic.com/identityServerFRA/',
+    API_BASE_URL: 'https://cortextest.singleclic.com/identityServerFRA/api',
     DOMAIN: 'https://identity-ui-iota.vercel.app/',
     DEV_MODE: false
   },
-  
+
   images: {
     domains: []
   },
+
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+      apexcharts: path.resolve(
+        __dirname,
+        './node_modules/apexcharts-clevision'
+      )
     }
 
     return config
